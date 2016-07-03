5ff652536ebc7632031c494e99c78dd34cfff611
Gerald Oliva <ryumaster001@yahoo.com>
2016-07-03 19:05:59 -0400

    User Story 6 completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :registered_applications, except: [:edit, :update]
 
   devise_for :users
+
+  namespace :api, defaults: {format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
   
   root 'welcome#index'
 end
